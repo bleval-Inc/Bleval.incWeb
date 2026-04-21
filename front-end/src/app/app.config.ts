181f1983f971438e1418a72c3b29759c88347c26
Tounga Saidou <bleval.inc@zohomail.com>
2026-04-21 23:21:21 +0200

    netlify-improvements

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig } from '@angular/core'
 import { provideRouter } from '@angular/router'
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
-import { provideClientHydration } from '@angular/platform-browser'
 import { provideHttpClient, withFetch } from '@angular/common/http'
 import { routes } from './app.routes'
 
@@ -9,7 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideClientHydration(),
     provideHttpClient(withFetch()),
   ],
 }
